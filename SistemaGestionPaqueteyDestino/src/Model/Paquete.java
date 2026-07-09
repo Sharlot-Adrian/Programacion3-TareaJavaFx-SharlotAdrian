bd1a01b39f8ee37d16cb4daf2d8aa6e18bb666e9
Sharlot <sharlot_adrian@ucne.edu.do>
2026-07-09 12:24:16 -0400

    Creacion de la clase Paquete

--- SistemaGestionPaqueteyDestino/src/Model/Paquete.java
+++ SistemaGestionPaqueteyDestino/src/Model/Paquete.java
@@ -1,5 +1,42 @@
 package Model;
 
 public class Paquete {
-    
+
+    private String codigo;
+    private String destinatario;
+    private double precio;
+    private String destino;
+
+    public Paquete(String codigo, String destinatario, double precio, String destino){
+        setCodigo(codigo);
+        setDestinatario(destinatario);
+        setPrecio(precio);
+        setDestino(destino);
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+    public String getDestinatario() {
+        return destinatario;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    public String getDestino() {
+        return destino;
+    }
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+    public void setDestinatario(String destinatario) {
+        this.destinatario = destinatario;
+    }
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
 }

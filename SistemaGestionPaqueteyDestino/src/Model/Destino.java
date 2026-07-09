16929b0ec23ba509d67cc652a3356d54421fb8ae
Sharlot <sharlot_adrian@ucne.edu.do>
2026-07-09 12:20:21 -0400

    Se agrego la clase Destino

---
+++ SistemaGestionPaqueteyDestino/src/Model/Destino.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class Destino {
+    private String nombre;
+
+    public Destino(String nombre){
+        setNombre(nombre);
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+}

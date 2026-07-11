e7a030f782f606159070a8874ab804ae7c666103
Sharlot <sharlot_adrian@ucne.edu.do>
2026-07-11 15:29:07 -0400

    Se establecieron las conexiones entre pantallas

--- SistemaGestionPaqueteyDestino/src/Controller/ControllerPantallaPrincipal.java
+++ SistemaGestionPaqueteyDestino/src/Controller/ControllerPantallaPrincipal.java
@@ -1,4 +1,5 @@
 package controller;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuBar;
@@ -20,10 +21,13 @@ public class ControllerPantallaPrincipal {
     @FXML
     void abrirConsultaDestino(ActionEvent event) {
 
+        Navegacion.abrirVentana("/view/consultaDestinos.fxml", "Consulta de Destinos");
     }
 
     @FXML
     void abrirConsultaPaquete(ActionEvent event) {
+
+        Navegacion.abrirVentana("/view/consultaPaquetes.fxml", "Consulta de Paquetes");
 
     }
 
@@ -35,17 +39,19 @@ public class ControllerPantallaPrincipal {
     @FXML
     void abrirRegistroDestino(ActionEvent event) {
 
+        Navegacion.abrirVentana("/view/registroDestino.fxml", "Registro de Destinos");
+
     }
 
     @FXML
     void abrirRegistroPaquete(ActionEvent event) {
 
-        Navegacion.abrirVentana("/view/consultaPaquetes.fxml", "Consulta de paquetes");
+        Navegacion.abrirVentana("/view/registroPaquete.fxml", "Registro de paquetes");
 
     }
     
     @FXML
     void salir(ActionEvent event) {
-
+        Platform.exit();
     }
 }

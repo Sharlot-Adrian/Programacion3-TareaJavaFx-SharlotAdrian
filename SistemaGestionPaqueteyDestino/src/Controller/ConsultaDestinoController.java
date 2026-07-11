1a61866e9310246cdd1a6d75d1c49cdacf7625a3
Sharlot <sharlot_adrian@ucne.edu.do>
2026-07-11 14:27:56 -0400

    Se creo el archivo fxml de la parte de consulta de destinos y se agrego su controller

---
+++ SistemaGestionPaqueteyDestino/src/Controller/ConsultaDestinoController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.text.Text;
+
+public class ConsultaDestinoController {
+
+    @FXML
+    private Button bttCargarArchivo;
+
+    @FXML
+    private Label lblMensaje;
+
+    @FXML
+    private TableView<String> tbvwDestinos;
+
+    @FXML
+    private Text txtTitulo;
+
+    @FXML
+    void cargarArchivo(ActionEvent event) {
+
+    }
+
+}
+

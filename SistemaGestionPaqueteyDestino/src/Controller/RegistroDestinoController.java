865fd302ffecc0dacddbc4793f3e84f965c2789e
Sharlot <sharlot_adrian@ucne.edu.do>
2026-07-11 14:00:32 -0400

    Modificaciones en el archivo fxml de la parte del registro de destinos y creacion del controller

---
+++ SistemaGestionPaqueteyDestino/src/Controller/RegistroDestinoController.java
@@ -0,0 +1,50 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class RegistroDestinoController {
+
+    @FXML
+    private Button bttCargarArchivo;
+
+    @FXML
+    private Button bttGuardarArchivo;
+
+    @FXML
+    private Label lblMensaje;
+
+    @FXML
+    private Label lblNombreDestino;
+
+    @FXML
+    private TableView<String> lstvwDestinosRegistrados;
+
+    @FXML
+    private TextField tfNombreDestino;
+
+    @FXML
+    private Text txtDestino;
+
+    @FXML
+    public void initialize(){
+        //lstvwDestinosRegistrados.getItems().add();
+    }
+
+    @FXML
+    void cargarArchivo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void guardarArchivo(ActionEvent event) {
+
+    }
+
+}
+
